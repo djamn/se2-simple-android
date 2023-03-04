50145e96e8b262d215d9d54d7475fbc3c90f245d
djamn <david@jamnig.net>
2023-03-04 23:26:07 +0100

    implemented some testing stuff to check out if replacement of TextView is working

--- app/src/main/java/net/jamnig/david/se2_simple_android/MainActivity.java
+++ app/src/main/java/net/jamnig/david/se2_simple_android/MainActivity.java
@@ -3,6 +3,9 @@ package net.jamnig.david.se2_simple_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.editTextNumber);
+                String number = textView.getText().toString();
+
+                TextView outputLocale = findViewById(R.id.textView5);
+                outputLocale.setText(number);
+
+            }
+        });
     }
+
 }//
