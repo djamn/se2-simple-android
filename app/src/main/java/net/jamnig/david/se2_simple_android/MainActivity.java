0a052dec010a03ca8a2d50f6ef1f70de43c1a4c1
djamn <david@jamnig.net>
2023-03-05 23:36:48 +0100

    implemented first task - tcp socket & thread for getting an answer of the server

--- app/src/main/java/net/jamnig/david/se2_simple_android/MainActivity.java
+++ app/src/main/java/net/jamnig/david/se2_simple_android/MainActivity.java
@@ -3,9 +3,12 @@ package net.jamnig.david.se2_simple_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView textView = findViewById(R.id.editTextNumber);
-                String number = textView.getText().toString();
-
-                TextView outputLocale = findViewById(R.id.textView5);
-                outputLocale.setText(number);
-
-            }
+        findViewById(R.id.button).setOnClickListener(view -> {
+            TextView textView = findViewById(R.id.editTextNumber);
+            String number = textView.getText().toString();
+            sendInput(number);
+            System.out.println("Print");
         });
     }
 
+    private void sendInput(String inputUser) {
+        new Thread(() -> {
+            String outputServer;
+            try {
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                outToServer.writeBytes(inputUser + '\n');
+
+                outputServer = inFromServer.readLine();
+                System.out.println("Debug - Output from server: " + outputServer);
+                TextView outputLocale = findViewById(R.id.textView5);
+                outputLocale.setText(outputServer);
+
+                clientSocket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }//
