ecdd4192bd910d257ea4b600e6fe3bfcf4e664c3
djamn <david@jamnig.net>
2023-03-06 11:52:12 +0100

    added some comments

--- app/src/main/java/net/jamnig/david/se2_simple_android/MainActivity.java
+++ app/src/main/java/net/jamnig/david/se2_simple_android/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Clicklistener für Send & Calculate Button
         findViewById(R.id.buttonSend).setOnClickListener(view -> {
             sendInput(getTextFieldNumber());
         });
@@ -30,19 +31,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendInput(String inputUser) {
+        // Neuer Thread für Socket
         new Thread(() -> {
             String outputServer;
             try {
-                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212); // Erstellt neuen Socket
 
+                // BufferedReader für Input & Output (vom und zum Server)
                 DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
                 BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+                // Übermittelt den Methodenkopf String zum Server (\n für Ende der Übermittlung)
                 outToServer.writeBytes(inputUser + '\n');
 
+                // Liest Output des Servers
                 outputServer = inFromServer.readLine();
                 System.out.println("Debug - Output from server: " + outputServer);
-                ((TextView) findViewById(R.id.textViewButtonOutput)).setText(outputServer);
+                ((TextView) findViewById(R.id.textViewButtonOutput)).setText(outputServer); // Übergibt den Output an ein TextView
 
                 clientSocket.close();
             } catch (Exception e) {
@@ -56,13 +61,15 @@ public class MainActivity extends AppCompatActivity {
         if (!inputValue.equals("")) {
             int[] arr = new int[inputValue.length()];
 
+            // String -> Einzelnen Int Values
             for (int i = 0; i < inputValue.length(); i++) {
                 arr[i] = Character.getNumericValue(inputValue.charAt(i));
             }
 
+            // Holt sich die Indizes der gemeinsamen Teiler
             String factorIndizes = getFactors(arr).toString();
 
-            ((TextView) findViewById(R.id.textViewButtonOutput)).setText(String.format(getString(R.string.output_factor_indizes_title), factorIndizes));
+            ((TextView) findViewById(R.id.textViewButtonOutput)).setText(String.format(getString(R.string.output_factor_indizes_title), factorIndizes)); // Ersetzt Output-Textview
         } else {
             ((TextView) findViewById(R.id.textViewButtonOutput)).setText(R.string.valid_number_error);
         }
@@ -71,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
     private StringBuilder getFactors(int[] arr) {
         StringBuilder builder = new StringBuilder();
+        // Liest 1x von vorne bis hinten (2 Pointer, einer i und einer i+1,
+        // welcher jedes i durchiteriert und nach arr[i] % arr[j] Ausschau hält
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > 1 && arr[j] > 1) {
@@ -81,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+        // Gleicheres wie oben nur von hinten nach vorne
         for (int i = arr.length - 1; i >= 0; i--) {
             for (int j = i - 1; j >= 0; j--) {
                 if (arr[i] > 1 && arr[j] > 1) {
