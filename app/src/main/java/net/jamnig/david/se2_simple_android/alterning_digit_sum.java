65e18bf15acdb004d2aed528eee875c674e98fbb
djamn <david@jamnig.net>
2023-03-16 09:26:07 +0100

    small fix

--- app/src/main/java/net/jamnig/david/se2_simple_android/alterning_digit_sum.java
+++ app/src/main/java/net/jamnig/david/se2_simple_android/alterning_digit_sum.java
@@ -11,7 +11,7 @@ public class alterning_digit_sum {
         int digitSum = numericalValues[0];
 
         for (int i = 1; i < numericalValues.length; i++) {
-            if (numericalValues[i] % 2 == 0) {
+            if (i % 2 == 0) {
                 digitSum += numericalValues[i];
             } else {
                 digitSum -= numericalValues[i];
